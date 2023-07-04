3bef79f5092d8e03c799158d075ba6514837a469
Eddy Ding <edwarding04@gmail.com>
2023-07-04 17:35:06 -0400

    Complete 2.2 and 2.3

--- course-info/src/components/Content.js
+++ course-info/src/components/Content.js
@@ -1,9 +1,12 @@
 const Content = ({parts}) => {
+    const total = parts.reduce((i, part) => i + part.exercises, 0);
     return(
         <>
             {parts.map(({id, name, exercises}) =>
                 (<div key={id}>{name} {exercises}</div>)
             )}
+            
+            <div><strong>total of {total} exercises</strong></div>
         </>
     )
 }
