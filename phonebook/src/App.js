08e057d08b1ce96e99bd1ce700e47093e4b62eac
Eddy Ding <edwarding04@gmail.com>
2023-07-05 19:56:22 -0400

    Complete 2.8

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,20 +2,25 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '704-423-3094' }
   ]) 
-  const [newName, setNewName] = useState('')
-  const updateNameText = (event) => {
-    console.log(event.target.value);
-    setNewName(event.target.value);
-  }
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const updateText = (event, setFunction) => 
+    setFunction(event.target.value);
+
+
 
   const addPerson = (event) => {
     event.preventDefault();
     const nameExists = persons.reduce((exists, person) => person.name === newName, false);
 
     if(!nameExists){
-      const newPerson = {name: newName};
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+      };
       setPersons(persons.concat(newPerson));
     }
     else alert(`${newName} is already in the phonebook.`)
@@ -23,22 +28,31 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit = {addPerson}>
         <div>
           name: <input 
-           value = {newName}
-           onChange= {updateNameText}
+            value = {newName}
+            onChange= {event => updateText(event, setNewName)}
+            required
           />
         </div>
         <div>
-          <button onClick = {addPerson} type="submit">add</button>
+          number: <input
+            value = {newNumber}
+            onChange= {event => updateText(event, setNewNumber)}
+            pattern= "(\d{3}([\-]?)\d{3}([\-]?)\d{4})"
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map(person =>
           <div>
-            {person.name}
+            {person.name} {person.number}
           </div>)}
         </div>
     </div>
